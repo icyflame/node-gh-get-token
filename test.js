31d68fcfaecf0a6eb7a288a097624f046a281651
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-16 20:21:48 +0530

    Added the test script.
    - Can also be used as an example
    - The Client ID and Client secrets are fudged

---
+++ test.js
@@ -0,0 +1,9 @@
+config_obj = {
+	"note": "A test application",
+	"client_id": "453662cdf983c1d839a1",
+	"client_secret": "48e1bd83f6576d0ac81db6335e929a729a355d04"
+}
+
+token = require('./')(config_obj);
+
+// console.log(token);
