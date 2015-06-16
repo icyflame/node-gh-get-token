4d0d0c016a0afad0105a79497a0dbfe66c704a2f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-16 20:21:22 +0530

    Add the main JS script

---
+++ index.js
@@ -0,0 +1,122 @@
+module.exports = function(config_obj){
+
+	/* keys in config_obj
+		* scopes : Array
+		* note : OAuth Token Note
+		* note_url : OAuth Token Note URL
+		* client_id : OAuth client ID
+		* client_secret : OAuth client secret
+		* single_auth : Default => true
+		*/
+		var clc = require('cli-color');
+
+		var SINGLE_AUTH = false;
+		var CLIENT_INFO = false;
+		var NOTE_INFO = false;
+
+		if(config_obj.client_id && config_obj.client_secret)
+
+			CLIENT_INFO = true;
+
+		if(config_obj.note)
+
+			NOTE_INFO = true;
+
+		if(!NOTE_INFO && !CLIENT_INFO){
+			console.log(clc.red("This authentication cannot be performed."));
+			console.log("You must provide either Note or the Client ID and Secret Key.");
+			return;
+		}
+
+		if(NOTE_INFO && !CLIENT_INFO)
+
+			SINGLE_AUTH = true;
+
+		if(CLIENT_INFO)
+
+			SINGLE_AUTH = config_obj.single_auth
+
+		console.log(clc.blue("Welcome to the one-time-authentication module."));
+		console.log("This module will take your github username and password\nand exchange it for an OAuth token");
+
+		var username, password;
+
+		var read = require('read');
+		var request = require('request-json');
+		var pkg = require('./package.json');
+
+		read({
+
+			prompt: "Enter your GitHub username: ",
+
+		}, function(err, result, isDef){
+			username = result;
+
+			read({
+
+				prompt: "Enter your GitHub password: ",
+				silent: true
+
+			}, function(err, result, isDef){
+				password = result;
+				var client = request.createClient("https://api.github.com/");
+				client.setBasicAuth(username, password);			
+
+				var data = {
+					"scopes": config_obj.scopes,
+					"note": config_obj.note,
+					"note_url": config_obj.note_url,
+					"client_id": config_obj.client_id,
+					"client_secret": config_obj.client_secret,
+				};
+
+				if(SINGLE_AUTH){
+
+					// console.log("Entered single auth!");
+
+					client.post("authorizations", data, function(err, res, body){
+
+						if(res.statusCode == 201 && !err){
+
+							console.log(clc.green("New token created!"));
+							console.log("Visit https://github.com/settings/tokens in your browser to revoke access.");							
+							return body.token;
+						}
+
+						else{
+							console.log("HTTP Status Code: " + res.statusCode);
+							console.log("We encountered an error in authentication!");
+							return;
+						}
+					});
+				}
+
+				else{
+					// console.log("Entered get-or-create auth!");
+					
+					client.put("authorizations/clients/" + config_obj.client_id, data, function(err, res, body){
+
+						if(res.statusCode == 201 && !err){
+
+							console.log(clc.green("New token created!"));
+							console.log("Visit https://github.com/settings/developers in your browser to revoke access.");
+							return body.token;
+
+						}
+
+						else
+							if(res.statusCode == 200 && !err){
+								console.log(clc.green("Existing token returned!"));
+								console.log("Visit https://github.com/settings/developers in your browser to revoke access.");
+								return body.token;
+							}
+							else{
+								console.log("HTTP Status Code: " + res.statusCode);
+								console.log("We encountered an error in authentication!");
+							}
+						});
+				}
+			});
+});
+
+}
